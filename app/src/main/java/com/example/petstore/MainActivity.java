19916e094f65eeba545644270eeed6e3aa194ce9
sandrescano <sandrescano@soyucn.edu.co>
2022-03-23 20:13:49 -0500

    Add layout drawable activity

--- app/src/main/java/com/example/petstore/MainActivity.java
+++ app/src/main/java/com/example/petstore/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.petstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnlogin);
+        EditText txtEmail = findViewById(R.id.txtEmail);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        TextView txtError = findViewById(R.id.txtError);
+
+        btnLogin.setOnClickListener(view -> {
+            String email = txtEmail.getText().toString();
+            String password = txtPassword.getText().toString();
+            if(getString(R.string.user_name).equals(email) && getString(R.string.user_password).equals(password)){
+                Intent switchActivity = new Intent(this, LayoutDrawableActivity.class);
+                startActivity(switchActivity);
+                txtError.setText("");
+            }
+            else{
+                txtError.setText(getString(R.string.login_error));
+            }
+        });
     }
 }
